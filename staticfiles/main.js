9a3996d0af972be07b95e1fd8ea1c81d83f692ef
Kimkim787 <129140224+Kimkim787@users.noreply.github.com>
2024-05-05 17:31:31 +0800

    Add files via upload

---
+++ staticfiles/main.js
@@ -0,0 +1,48 @@
+$('.studentlife-btn').click(function() {
+    console.log("clicked");
+    $('.studentlife').toggleClass("show"); 
+    $('.first').toggleClass("rotate");
+});
+
+$('.scholar-btn').click(function() {
+    console.log("clicked");
+    $('.scholar-show').toggleClass("show2"); 
+    $('.side-nav ul .second').toggleClass("rotate");
+});
+
+$('.jobplace-btn').click(function() {
+    console.log("clicked");
+    $('.jobplace-show').toggleClass("show3"); 
+    $('.side-nav ul .third').toggleClass("rotate");
+});
+
+$('.student_disc-btn').click(function() {
+    console.log("clicked");
+    $('.student_disc-show').toggleClass("show4"); 
+    $('.side-nav ul .fourth').toggleClass("rotate");
+});
+
+$('.guide-btn').click(function() {
+    $('.guide-show').toggleClass("show5"); 
+    $('.side-nav ul .fifth').toggleClass("rotate");
+});
+
+$('.alumni-btn').click(function() {
+    $('.alumni-show').toggleClass("show6"); 
+    $('.side-nav ul .sixth').toggleClass("rotate");
+});
+
+$('.community-btn').click(function() {
+    $('.community-show').toggleClass("show7"); 
+    $('.side-nav ul .seventh').toggleClass("rotate");
+});
+
+$('.student_org-btn').click(function() {
+    $('.student_org-show').toggleClass("show8"); 
+    $('.side-nav ul .eight').toggleClass("rotate");
+});
+
+$('.medical-btn').click(function() {
+    $('.medical-show').toggleClass("show9"); 
+    $('.side-nav ul .ninth').toggleClass("rotate");
+});
